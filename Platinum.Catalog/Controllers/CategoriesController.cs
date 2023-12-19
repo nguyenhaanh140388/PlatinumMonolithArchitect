66c9e348ab4b2520c1381a003b9b1006e2d4d1bc
nguyenhaanh14031988@gmail.com <nguyenhaanh14031988@gmail.com>
2023-12-19 15:44:49 +0700

    Add  test controller to Catalog module

---
+++ Platinum.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Platinum.Core.Common;
+
+namespace Platinum.Catalog.Controllers
+{
+    public class CategoriesController : BaseController
+    {
+        [HttpGet]
+        [Route("get-animals")]
+        public ActionResult<List<string>> GetListAnimal()
+        {
+            return Ok(new List<string>() { "dog", "cat", "fish" });
+
+        }
+    }
+}
